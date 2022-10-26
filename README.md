faa96a55cea0c0d5b6183424af58ca6d4a84a53d
KwangSensei <exiastrike@hotmail.com>
2022-10-26 09:58:49 +0700

    Add wiki links in README/Remove secret-key

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Fiction-Fans
+# Fiction Fans
+
+**Fiction Fans** is a web application for those who want to read, write story online.
+
+# Project Document
+- All project documents are in the [Project Wiki](../../wiki/Home).
+- [Vision Statement](../../wiki/Vision%20Statement)
+- [Requirements](../../wiki/Requirements)
+- [User Stories](../../wiki/User%20Stories)
--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vf(l1htp8=!)yq_#wt5wb@k&_xe=5rqf0$0e^kf54!9q*v*e5@'
+SECRET_KEY = config("SECRET_KEY", default="missing-secret-key", cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = config("TIME_ZONE", default="Asia/Bangkok", cast=str)
 
 USE_I18N = True
 
