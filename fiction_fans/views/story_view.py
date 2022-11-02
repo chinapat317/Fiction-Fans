4fc17de28b28923bcf947f5294675735f9c3fde5
inwpolol <Pichaiyut_2001@hotmail.com>
2022-11-02 15:33:47 +0700

    Fix import model.story

--- fiction_fans/views/story_view.py
+++ fiction_fans/views/story_view.py
@@ -1,14 +1,7 @@
 from django.views import generic
 
 # importing module
-import sys
-
-# appending a path
-sys.path.append('../models')
-
-from story import Story
-
-# Create your views here.
+import ..models.story import Story
 
 
 class StoryView(generic.DetailView):
