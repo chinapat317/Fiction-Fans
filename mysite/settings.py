e265ba1369b8f41419777e583cef9c7277f990a9
KwangSensei <exiastrike@hotmail.com>
2022-11-08 14:30:33 +0700

    Fix typo

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'fiction_fans.apps.FictionFansConfig',
-    'django_editorjs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "fiction_fans.apps.FictionFansConfig",
+    "django_editorjs",
 ]
 
 MIDDLEWARE = [
