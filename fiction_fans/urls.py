ca322058e8c91037aa9a7f64468adce5b02f82e2
KwangSensei <exiastrike@hotmail.com>
2022-11-08 14:04:11 +0700

    Fix wrong URL path name

--- fiction_fans/urls.py
+++ fiction_fans/urls.py
@@ -4,6 +4,6 @@ from .views import fiction_view, homepage_view
 urlpatterns = [
     # path("fiction/<int:fiction_id>/<int:chapter_id>", chapter_view.ChapterView.as_view(), name="chapter_view"),
     path("", homepage_view.homepage, name="homepage"),
-    path("novel/<int:fiction_id>/", fiction_view.fiction_view, name="fiction_view"),
-    path("novel/<int:fiction_id>/<int:chapter_id>", fiction_view.chapter_view, name="chapter_view"),
+    path("fiction/<int:fiction_id>/", fiction_view.fiction_view, name="fiction_view"),
+    path("fiction/<int:fiction_id>/<int:chapter_id>", fiction_view.chapter_view, name="chapter_view"),
 ]
