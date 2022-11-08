715c126aa705d467a20d216fb4ed687ff852dcaf
KwangSensei <exiastrike@hotmail.com>
2022-11-08 13:45:28 +0700

    Fix wrong class name

--- fiction_fans/admin.py
+++ fiction_fans/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import FictionTitle, FictionChapter
+from .models.fiction_model import FictionTitle, FictionChapter
 
 # Register your models here.
+
 admin.site.register(FictionChapter)
 admin.site.register(FictionTitle)
