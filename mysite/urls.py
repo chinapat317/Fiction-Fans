b492b86cf6c103ee17274cfab1f3cbd83cf0635b
inwpolol <Pichaiyut_2001@hotmail.com>
2022-11-09 13:39:05 +0700

    Add auth and signup path

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import signup
 
 urlpatterns = [
     path("fiction/", include("fiction_fans.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', signup, name='signup')
 ]
