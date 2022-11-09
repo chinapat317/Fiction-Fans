29f45d66d8c8ad74bb912a6b0c5400c51368ef13
Chinapat <chinapat.r@ku.th>
2022-11-10 00:57:13 +0700

    fixing url action in template and add redirect when finishing create title or chapter

--- fiction_fans/views/fiction_create_view.py
+++ fiction_fans/views/fiction_create_view.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from ..forms.fiction_form import FictionForm, ChapterForm
 
-# Create your views here.
-
-
 def create_fiction(request):
+    """
+    For creating fiction title page.
+    """
     form = FictionForm(request.POST or None)
     template_name = "fiction_fans/fiction_create.html"
     context = {
@@ -15,11 +15,14 @@ def create_fiction(request):
     if request.method == "POST":
         if form.is_valid():
             fiction = form.save()
-            return HttpResponseRedirect(reverse("fiction_view", args=(fiction.id,)))
+            return HttpResponseRedirect(reverse("fiction_fans:fiction_view", args=(fiction.id,)))
     return render(request, template_name, context=context)
 
 
 def create_chapter(request):
+    """
+    For creating chapter in fiction title.
+    """
     form = ChapterForm(request.POST or None)
     template_name = "fiction_fans/chapter_create.html"
     context = {
@@ -28,5 +31,7 @@ def create_chapter(request):
     if request.method == "POST":
         if form.is_valid():
             chapter = form.save()
-            return HttpResponseRedirect(reverse("chapter_view", args=(chapter.novel_title.id, chapter.id,)))
+            fiction_pk = chapter.fiction_title_id
+            chapter_pk = chapter.id
+            return HttpResponseRedirect(reverse("fiction_fans:chapter_view", args=[fiction_pk, chapter_pk]))
     return render(request, template_name, context=context)
