f31b0b0839daeeea1cabc09a4b592435eb7af5b0
Chinapat <chinapat.r@ku.th>
2022-11-20 13:22:34 +0700

    Comment feature
    - Add comment models.
    - Add comment view for create comment model.
    - Comment obj can edit in admin page.
    - user can't comment if not login.
    - create message in comment view but not show in login page yet.

--- fiction_fans/models/comment_model.py
+++ fiction_fans/models/comment_model.py
@@ -1,8 +1,9 @@
 from django.db import models
 from .fiction_model import FictionChapter
+from django.conf import settings
 
 class Comment(models.Model):
     """Create comment in chapter and keep it in sqlite."""
     chapter = models.ForeignKey(FictionChapter, on_delete=models.CASCADE)
-    username = models.CharField(max_length=30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
--- fiction_fans/views/comment.py
+++ fiction_fans/views/comment.py
@@ -9,10 +9,13 @@ def comment(request, fiction_pk, chapter_pk):
     user = request.user
     if not user.is_authenticated:
         messages.add_message(request, messages.INFO, 'Please login before comment.')
-        return HttpResponseRedirect('../../../account/login/')
+        return HttpResponseRedirect('../../../../accounts/login/')
     chapter = get_object_or_404(FictionChapter, pk=chapter_pk)
     comment_text = request.POST.get('comment_text')
+    #################################################################
+    # delete when finish
     file = open("log.txt", "w")
     file.write("comment_text = {}".format(request.POST.get("comment_text")))
-    chapter.comment_set.create(username=user.get_username(), text=comment_text)
+    ###################################################################
+    chapter.comment_set.create(user=user, text=comment_text)
     return HttpResponseRedirect(reverse('fiction_fans:chapter_view', args=[fiction_pk, chapter_pk]))
