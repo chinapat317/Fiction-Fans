3ec09225803082c18fc6ef72f5d8a187912bc8f3
KwangSensei <exiastrike@hotmail.com>
2022-11-20 15:56:23 +0700

    Add delete URLs

--- fiction_fans/urls.py
+++ fiction_fans/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from .views import fiction_page_view, homepage_view, fiction_create_view, fiction_edit_view
+from .views import homepage_view, fiction_page_view, fiction_create_view, fiction_edit_view, fiction_delete_view
 
 
 app_name = "fiction_fans"
@@ -13,11 +13,15 @@ urlpatterns = [
 
     path("<int:fiction_id>/edit/", fiction_edit_view.edit_fiction, name="fiction_edit"),
 
+    path("<int:fiction_id>/del/", fiction_delete_view.delete_fiction, name="fiction_del"),
+
     path("<int:fiction_pk>/<int:chapter_pk>/", fiction_page_view.ChapterView.as_view(), name="chapter_view"),
 
     path("createchap/", fiction_create_view.create_chapter, name="chapter_create"),
 
     path("<int:fiction_id>/<int:chapter_id>/edit/", fiction_edit_view.edit_chapter, name="chapter_edit"),
+
+    path("<int:fiction_id>/<int:chapter_id>/del/", fiction_delete_view.delete_chapter, name="chapter_del"),
 
     path("imageUPload/", csrf_exempt(fiction_page_view.upload_image_view)),
 ]
