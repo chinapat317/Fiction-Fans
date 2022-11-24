f5d47e1ecddc02131f45d56962ae1298854f5c9e
Chinapat <chinapat.r@ku.th>
2022-11-24 15:10:34 +0700

    show comment feature

--- fiction_fans/models/comment_model.py
+++ fiction_fans/models/comment_model.py
@@ -1,9 +1,12 @@
 from django.db import models
 from .fiction_model import FictionChapter
 from django.conf import settings
+from django.utils import timezone
 
 class Comment(models.Model):
     """Create comment in chapter and keep it in sqlite."""
     chapter = models.ForeignKey(FictionChapter, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
+    comment_date = models.DateTimeField(auto_now_add=timezone.now())
+    
