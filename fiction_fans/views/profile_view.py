606336cb215324d0c96a05b3d975e2c792059982
inwpolol <Pichaiyut_2001@hotmail.com>
2022-11-26 20:44:29 +0700

    Add filter fiction by creater in profile page

--- fiction_fans/views/profile_view.py
+++ fiction_fans/views/profile_view.py
@@ -4,11 +4,11 @@ from django.views import generic
 
 # Create your views here.
 
-
 class Profile(generic.ListView):
     """Create view for homepage list all fiction's title."""
     template_name = "fiction_fans/profile_page.html"
     context_object_name = "fictions"
 
     def get_queryset(self):
-        return FictionTitle.objects.all()
+        if self.request.user.is_authenticated:
+            return FictionTitle.objects.filter(user=self.request.user)
