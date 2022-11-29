b74937ca8bfc9614e4c87fbcf0110e2bce15322e
inwpolol <Pichaiyut_2001@hotmail.com>
2022-11-29 14:52:06 +0700

    Fix register

--- mysite/views.py
+++ mysite/views.py
@@ -10,11 +10,11 @@ def signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
             username = form.cleaned_data.get("username")
             raw_passwd = form.cleaned_data.get("password")
             user = authenticate(username=username,password=raw_passwd)
             login(request, user)
+            form.save()
         return redirect("/accounts/login")
         # what if form is not valid?
         # we should display a message in signup.html
